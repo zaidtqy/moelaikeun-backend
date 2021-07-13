488be622782a4b494bd76522ac159e5bd1fe59bc
zaidtqy <mzaidtaqy04@gmail.com>
2021-07-13 20:40:06 +0700

    Deployment Command Auth

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'DashboardController@index')->name('dashboard');
 
-Auth::routes(['register' => false]);
+// Auth::routes(['register' => false]);
 
 Route::get('products/{id}/gallery', 'ProductController@gallery')
     ->name('products.gallery');
